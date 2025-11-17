1fa214e6fde0a68dcd8a9b8bbf8680fb7f928989
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-11-17 22:20:54 +0700

    style: fix overflow codeblock

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -54,13 +54,13 @@ export default function RootLayout({ children }) {
       <body className={`${poppins.variable} ${jetbrains.variable} ${arabic.variable} antialiased`}>
         <SidebarProvider>
           <AppSidebar fileTree={fileTree} />
-          <SidebarInset>
+          <SidebarInset className="min-w-0">
             <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
               <SidebarTrigger className="-ml-1" />
               <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
               <AppBreadcrumb allMarkdownFiles={allMarkdownFiles} />
             </header>
-            <div className="flex flex-1 flex-col gap-4 p-4">
+            <div className="flex flex-1 flex-col p-2">
               <div className="min-h-[100vh] flex-1 rounded-xl border p-4 md:min-h-min">
                 {children}
               </div>
